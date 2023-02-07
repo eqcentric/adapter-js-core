49995d468203a1b34a675a094b759d7f716e3849
Nghia Le <nghiale@Nghias-MacBook-Pro.local>
2023-02-07 17:26:28 +0700

    update interface

--- src/Dto/CollectionInterface.ts
+++ src/Dto/CollectionInterface.ts
@@ -1,6 +1,10 @@
+import { Criteria, TypeMarkField } from "./InternalDto";
+
 export interface CollectionInterface {
   getName(): string;
   getPrimaryKeys(): Array<string>;
   getMongoIndexed(): Array<string>;
   getMongoScope(): string | null;
+  getPickFields(): Array<TypeMarkField>;
+  getCriterias(): Array<Criteria>;
 }
