78f0e6ee7fcf5181ebfec2fdad5f00fe17e42616
Nghia Le <nghiale@Nghias-MacBook-Pro.local>
2023-02-09 14:42:27 +0700

    fix type get options

--- src/Dto/CollectionInterface.ts
+++ src/Dto/CollectionInterface.ts
@@ -7,5 +7,5 @@ export interface CollectionInterface {
   getMongoScope(): string | null;
   getPickFields(): Array<TypeMarkField>;
   getCriterias(integrationId?: number): Array<Criteria>;
-  getOptions(): Array<Record<string, string>>;
+  getOptions(): Record<string, string>;
 }
