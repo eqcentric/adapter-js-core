404d03fbb041eb5a30b92cf2364a0ee1d7bb67de
nghia <124866887+vurask@users.noreply.github.com>
2023-08-17 00:14:24 +0700

    fix

--- src/Utils/Helpers.ts
+++ src/Utils/Helpers.ts
@@ -5,7 +5,7 @@ export class Helpers {
         return process.env.INTERNAL_CHANNEL === 'sqs';
     }
 
-    static sendNotifyCompleted (integrationId: number, apiUrl: string, entity: string, totalRecords: number, options: [] ): boolean {
+    static sendNotifyCompleted (integrationId: number, apiUrl: string, entity: string, totalRecords: number, options: any ): boolean {
         if ( Helpers.isSQS() ) {
             const noti = SqsNotification.getInstance(integrationId, apiUrl, options);
             noti.request(entity, totalRecords);
